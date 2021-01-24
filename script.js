375dea60b9df417454831c03893b69c0f8cb6f17
iansaf <iansaf@ga.github.io>
2021-01-24 10:02:54 +0800

    ian

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-$("#google").click(() => { 
+better_js_click("#google", () => {
     window.location.href = 'https://www.google.com/tw';
 });
 
-$("#youtube").click(() => {
+better_js_click("#youtube", () => {
     window.location.href = 'https://www.youtube.com.tw';
 });
 
-$("#Gmail").click(() => {
+better_js_click("#Gmail", () => {
     window.location.href = 'https://mail.google.com';
 });
 
-$("#google-map").click(() => {
+better_js_click("#google-map", () => {
     window.location.href = 'https://maps.google.com';
 });
 
-$("#minecraft").click(() => {
+better_js_click("#minecraft", () => {
     window.location.href = 'https://minecraft.net/zh-hant';
 });
 
-$("#minecraft-wiki").click(() => {
+better_js_click("#minecraft-wiki", () => {
     window.location.href = 'https://minecraft-zh.gamepedia.com/Minecraft_Wiki?variant=zh-tw';
 });
 
-$("#gamer").click(() => {
+better_js_click("#gamer", () => {
     window.location.href = 'https://www.gamer.com.tw';
 });
 
-$("#L1").click(() => {
+better_js_click("#L1", () => {
     window.location.href = 'https://iansaf.github.io/L1/index.html';
-})
+});
